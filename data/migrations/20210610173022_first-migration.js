8e4732a833095a975d6f1943df05641c33fea39b
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-06-10 19:33:40 +0200

    wip

--- data/migrations/20210610173022_first-migration.js
+++ data/migrations/20210610173022_first-migration.js
@@ -3,15 +3,15 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('zoos')
     .createTable('species')
-    .createTable()
-    .createTable()
+    .createTable('animals')
+    .createTable('zoo_animals')
 };
 
 exports.down = function(knex) {
   // we drop them in reverse order we created them
   return knex.schema
-    .dropTableIfExists()
-    .dropTableIfExists()
-    .dropTableIfExists()
+    .dropTableIfExists('zoo_animals')
+    .dropTableIfExists('animals')
+    .dropTableIfExists('species')
     .dropTableIfExists('zoos')
 };
